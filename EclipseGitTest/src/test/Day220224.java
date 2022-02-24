7876b0943c91b366e50644c3c1e33faf3e926f94
Wyna <98083311+Wyna2@users.noreply.github.com>
2022-02-24 22:08:38 +0900

    commit test

--- EclipseGitTest/src/test/Day220224.java
+++ EclipseGitTest/src/test/Day220224.java
@@ -5,7 +5,7 @@ public class Day220224 {
 	public static void main(String[] args) {
 		System.out.println("EclipseGitTest");
 		System.out.println("CommitAndPushTest");
-		System.out.println("=================");
+		System.out.println("================");
 		
 	}
 
